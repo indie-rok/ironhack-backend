d94d6a5247917012a57c856e77b0f3a9c82028d0
Emmanuel Orozco <indi3.rok@gmail.com>
2020-01-21 10:27:05 -0600

    added fix routing back end front end

--- src/client/App.js
+++ src/client/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import React, { Component } from "react";
+import "./app.css";
+import ReactImage from "./react.png";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+function Test() {
+  return <div>hello div</div>;
+}
 
 export default class App extends Component {
   state = { username: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
+    fetch("/api/getUsername")
       .then(res => res.json())
       .then(user => this.setState({ username: user.username }));
   }
@@ -15,8 +20,17 @@ export default class App extends Component {
     const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        {username ? (
+          <h1>{`Hello ${username}`}</h1>
+        ) : (
+          <h1>Loading.. please wait!</h1>
+        )}
         <img src={ReactImage} alt="react" />
+        <Router>
+          <Switch>
+            <Route path="/test" component={Test} />
+          </Switch>
+        </Router>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const os = require('os');
+const express = require("express");
+const os = require("os");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+mongoose
+  .connect(
+    "mongodb+srv://indie-rok:Cheers2u@ironhack-test-rqer6.gcp.mongodb.net/ironhack?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB...", err));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.use(express.static("dist"));
+app.get("/api/getUsername", (req, res) =>
+  res.send({ username: os.userInfo().username })
+);
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Listening on port ${process.env.PORT || 8080}!`)
+);
